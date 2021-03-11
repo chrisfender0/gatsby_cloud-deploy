6dba542190a31f8505cd07ad6b630a810b0d8bfe
Christian Juste <chrisfender0@gmail.com>
2021-03-10 23:37:27 -0800

    changed index page text, test hotdeploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
+    <div style={{ color: `black` }}>
+      <Header headerText="Hello World" />
+      <Header headerText="Gatsby is a piece of cake!" />
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
